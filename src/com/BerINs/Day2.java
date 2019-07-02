89ab0227021f7ca4efe13cf841ef259f7772a207
berior <1269921156@qq.com>
2019-07-02 20:41:01 +0800

    Initial commit

---
+++ src/com/BerINs/Day2.java
@@ -0,0 +1,24 @@
+package com.BerINs;
+
+/**
+ * Berior
+ * 2019-06-30 21:56
+ */
+public class Day2 {
+    public static void main(String[] args) {
+        int n1 = 129;
+        byte n2 = 3;
+        float n3 = n1 + n2;
+
+        double d1 = 3.14;
+        int d2 = (int)d1;
+        System.out.println(d2);
+
+        int i1 = 128;
+        byte b = (byte)i1;
+        System.out.println(b);
+
+        long l1 = 123;
+
+    }
+}
