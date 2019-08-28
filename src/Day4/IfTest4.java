f5749223a069a009f110a226fd698051078a0900
berior <1269921156@qq.com>
2019-08-28 22:47:20 +0800

    If语句测试题

--- src/Day4/IfTest4.java
+++ src/Day4/IfTest4.java
@@ -13,12 +13,23 @@ public class IfTest4 {
         int height = in.nextInt();
         System.out.println("请输入你的财富: (千万)");
         double wealth = in.nextDouble();
-        System.out.println("请输入你是否帅: (true/false)");
-        boolean isHandsome = in.nextBoolean();
+//        System.out.println("请输入你是否帅: (true/false)");
+//        boolean isHandsome = in.nextBoolean();
+//
+//        if (height >= 180 && wealth >= 1 && isHandsome) {
+//            System.out.println("我一定要嫁给他!!!");
+//        }else if(height >=180 || wealth >= 1 || isHandsome){
+//            System.out.println("嫁了吧，比上不足，比下有余。");
+//        }else{
+//            System.out.println("不嫁。");
+//        }
+        //方式二
+        System.out.println("请输入你是否帅: (是/否）");
+        String isHandsome = in.next();
 
-        if (height >= 180 && wealth >= 1 && isHandsome) {
+        if (height >= 180 && wealth >= 1 && isHandsome.equals("是")) {
             System.out.println("我一定要嫁给他!!!");
-        }else if(height >=180 || wealth >= 1 || isHandsome){
+        }else if(height >=180 || wealth >= 1 || isHandsome.equals("是")){
             System.out.println("嫁了吧，比上不足，比下有余。");
         }else{
             System.out.println("不嫁。");
