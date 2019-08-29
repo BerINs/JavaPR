0039195693a13349c07b12fbc8b2120b50cfe567
berior <1269921156@qq.com>
2019-08-29 20:31:16 +0800

    九九乘法表

--- src/Day5/ForForTest.java
+++ src/Day5/ForForTest.java
@@ -19,5 +19,12 @@ public class ForForTest {
             }
             System.out.println();
         }
+
+        for(int i = 1; i <= 4; i++) {
+            for(int j = 1; j <= 5 - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
