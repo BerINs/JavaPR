96028197bf4ee115cb6906f859b977d9c45b916d
berior <1269921156@qq.com>
2019-08-29 15:14:49 +0800

    While循环 Do-While循环

--- src/Day5/WhileTest.java
+++ src/Day5/WhileTest.java
@@ -14,5 +14,13 @@ public class WhileTest {
             i++;
         }
 
+        int num = 1;
+        do {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+            num++;
+        }while(num <= 100);
+
     }
 }
