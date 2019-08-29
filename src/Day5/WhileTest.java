0618433e7f887b3421d5ddf9c86ca907ef26e759
berior <1269921156@qq.com>
2019-08-29 15:05:01 +0800

    While循环

---
+++ src/Day5/WhileTest.java
@@ -0,0 +1,18 @@
+package Day5;
+
+/**
+ * BerINs
+ * 2019-08-29 14:58
+ */
+public class WhileTest {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i <= 100) {
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+
+    }
+}
