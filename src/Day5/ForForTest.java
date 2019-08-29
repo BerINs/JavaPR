1794127ec6b8cfcd30f2bd731a549cff86cfcbb8
berior <1269921156@qq.com>
2019-08-29 20:13:24 +0800

    FOR嵌套循环

---
+++ src/Day5/ForForTest.java
@@ -0,0 +1,23 @@
+package Day5;
+
+/**
+ * BerINs
+ * 2019-08-29 19:47
+ */
+public class ForForTest {
+    public static void main(String[] args) {
+        for(int j = 1; j <= 4; j++) {
+            for(int i = 1; i <= 6; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
