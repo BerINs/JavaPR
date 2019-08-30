4db668954d583a5b213d70e823277f7b58de7d4f
berior <1269921156@qq.com>
2019-08-30 21:47:37 +0800

    数组：初始化，长度

---
+++ src/Day6/ArrayTest1.java
@@ -0,0 +1,48 @@
+package Day6;
+
+public class ArrayTest1 {
+    public static void main(String[] args) {
+
+        //数组元素的默认初始化值
+        int[] arr = new int[4];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+
+        System.out.println();
+
+        short[] arr1 = new short[4];
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i]);
+        }
+
+        System.out.println();
+
+        float[] arr2 = new float[4];
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i]);
+        }
+
+        System.out.println(
+
+        );
+        char[] arr3 = new char[4];
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.print(arr3[i]);
+        }
+
+        System.out.println();
+
+        boolean[] arr4 = new boolean[5];
+        System.out.println(arr4[0]);
+
+        System.out.println();
+
+        String[] arr5 = new String[5];
+        System.out.println(arr5[0]);
+
+        //数组的内存解析
+
+
+    }
+}
