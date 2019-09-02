a166de278ecd9f9b47fc7a480f094332a67251cb
berior <1269921156@qq.com>
2019-09-02 14:11:09 +0800

    杨辉三角

---
+++ src/Day7/YangHuiTest.java
@@ -0,0 +1,26 @@
+package Day7;
+
+public class YangHuiTest {
+    public static void main(String[] args) {
+
+        //1.声明并初始化二维数组
+        int[][] yangHui = new int[10][];
+        //2.数组的元素赋值
+        for (int i = 0; i < yangHui.length; i++) {
+            yangHui[i] = new int[i + 1];
+            //2.1首尾元素赋值
+            yangHui[i][0] = yangHui[i][i] = 1;
+            //2.2给每行的非首尾元素赋值
+            for (int j = 1; j < yangHui[i].length - 1; j++) {
+                yangHui[i][j] = yangHui[i-1][j-1] + yangHui[i-1][j];
+            }
+        }
+        //3.遍历二维数组
+        for (int i = 0; i < yangHui.length; i++) {
+            for(int j = 0; j < yangHui[i].length; j++) {
+                System.out.print(yangHui[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
