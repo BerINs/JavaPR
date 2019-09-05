e96b208f182c5016241ac933606cb534d798e50d
berior <1269921156@qq.com>
2019-09-05 21:21:56 +0800

    数组的复制，反转，查找操作

--- src/Day7/ArrayTest2.java
+++ src/Day7/ArrayTest2.java
@@ -21,14 +21,31 @@ public class ArrayTest2 {
 //            arr[arr.length - i -1] = temp;
 //        }
         //方式二
-        for (int i =0, j = arr.length - 1; i < j; i++,j--) {
-            String temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
+//        for (int i =0, j = arr.length - 1; i < j; i++,j--) {
+//            String temp = arr[i];
+//            arr[i] = arr[j];
+//            arr[j] = temp;
+//        }
 
-        for (int i =0; i < arr.length; i++) {
-            System.out.print(arr[i] + "\t");
+//        for (int i =0; i < arr.length; i++) {
+//            System.out.print(arr[i] + "\t");
+//        }
+
+        //查找
+        //线性查找
+        String dest = "Bc";
+
+        boolean isFlag = true;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (dest.equals(arr[i])) {
+                System.out.println("找到了指定的元素，位置为：" + i);
+                isFlag = false;
+                break;
+            }
+        }
+        if (isFlag) {
+            System.out.println("很遗憾，没有找到哦！");
         }
     }
 }
