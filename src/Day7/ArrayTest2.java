71b23a24d77b4d5b2c7262555cf1c900580ca984
berior <1269921156@qq.com>
2019-09-05 21:11:04 +0800

    数组的反转操作

---
+++ src/Day7/ArrayTest2.java
@@ -0,0 +1,34 @@
+package Day7;
+
+import javax.sound.midi.Soundbank;
+
+public class ArrayTest2 {
+    public static void main(String[] args) {
+
+        String[] arr = new String[]{"JJ","DD","MM","BB","GG","AA"};
+
+        //数组的复制
+        String[] arr1 = new String[arr.length];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = arr[i];
+        }
+
+        //数组的反转
+        //方式一
+//        for (int i = 0; i < arr.length / 2; i++) {
+//            String temp = arr[i];
+//            arr[i] = arr[arr.length - i - 1];
+//            arr[arr.length - i -1] = temp;
+//        }
+        //方式二
+        for (int i =0, j = arr.length - 1; i < j; i++,j--) {
+            String temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        for (int i =0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
+        }
+    }
+}
