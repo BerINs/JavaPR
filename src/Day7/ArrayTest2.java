dc31cfd7d26f3c269a462e8d13395bd21d121904
berior <1269921156@qq.com>
2019-09-06 10:57:25 +0800

    数组的复制，反转，查找操作（包括二分法找到，必须要有序）

--- src/Day7/ArrayTest2.java
+++ src/Day7/ArrayTest2.java
@@ -8,10 +8,10 @@ public class ArrayTest2 {
         String[] arr = new String[]{"JJ","DD","MM","BB","GG","AA"};
 
         //数组的复制
-        String[] arr1 = new String[arr.length];
-        for (int i = 0; i < arr1.length; i++) {
-            arr1[i] = arr[i];
-        }
+//        String[] arr1 = new String[arr.length];
+//        for (int i = 0; i < arr1.length; i++) {
+//            arr1[i] = arr[i];
+//        }
 
         //数组的反转
         //方式一
@@ -31,6 +31,7 @@ public class ArrayTest2 {
 //            System.out.print(arr[i] + "\t");
 //        }
 
+
         //查找
         //线性查找
         String dest = "Bc";
@@ -47,5 +48,28 @@ public class ArrayTest2 {
         if (isFlag) {
             System.out.println("很遗憾，没有找到哦！");
         }
+
+        //二分法查找:必须要有序
+        //折半查找
+        int[] arr2 = new int[]{1,2,3,4,5,6,7,8,9,10};
+
+        int dest1 = 9;
+        int head = 0;
+        int end = arr2.length - 1;
+
+        while (head <= end) {
+
+            int middle = (head + end) / 2;
+
+            if (dest1 == arr2[middle]) {
+                System.out.println("找到了指定的元素，位置为：" + middle);
+                break;
+            }else if (arr2[middle] > dest1) {
+                end = middle - 1;
+            }else {
+                head = middle + 1;
+            }
+        }
+
     }
 }
