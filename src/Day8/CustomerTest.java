97a1e8514d6e9c7a4ea4fd4fc986d57ea369ea5e
berior <1269921156@qq.com>
2019-09-08 13:57:50 +0800

    类方法的声明和使用

--- src/Day8/CustomerTest.java
+++ src/Day8/CustomerTest.java
@@ -19,15 +19,32 @@ package Day8;
  * 返回值类型： 有 / 无
  * 有:必须在方法声明时，指定返回值的类型，同时，需要使用return关键字来返回返回值的常量或者变量
  *
- * 无: 使用void来表示,可以使用return;结束
+ * 无: 使用void来表示,可以使用return;结束   return后不可声明表达式
+ *
+ * 方法名：属于标识符，
+ * 形参是可以被调用的
+ *
+ * 方法体：方法功能的体现
+ *
+ * return关键字的使用：
+ *  1：使用范围：使用在方法体中
+ *  2：作用 结束方法
+ *         针对有返回值类型的方法，使用return数据方法返回所要的语句
+ *
+ *  方法的使用： 可以调用当前类的属性或方法
+ *              特殊的：方法A中又调用了A方法 （递归方法）
+ *              方法中不可以定义别的方法
+ *
  */
 
 public class CustomerTest {
     public static void main(String[] args) {
         Customer cust1 = new Customer();
         cust1.eat();
-    }
 
+        cust1.sleep(8);
+
+    }
 }
 
 class Customer {
