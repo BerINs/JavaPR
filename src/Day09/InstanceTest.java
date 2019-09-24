750d0565b07d54400b742dcf7a5cefca63849f34
berior <1269921156@qq.com>
2019-09-24 23:14:41 +0800

    面向对象

--- src/Day09/InstanceTest.java
+++ src/Day09/InstanceTest.java
@@ -1,5 +1,16 @@
 package Day09;
 
 public class InstanceTest {
+    public static void main(String[] args) {
+        Student[] stus = new Student[5];
+        stus[0] = new Student();
+        stus[1] = new Student();
+        System.out.println(stus[1]);
+    }
+
+
+}
+
+class Student{
 
 }
