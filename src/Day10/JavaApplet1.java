efd2957c99d2a146122ca36c03691044a6aa6913
berior <1269921156@qq.com>
2020-11-06 13:03:45 +0800

    Java Applet

---
+++ src/Day10/JavaApplet1.java
@@ -0,0 +1,10 @@
+package Day10;
+
+import java.awt.Graphics;
+import java.applet.Applet;
+
+public class JavaApplet1 extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("好好学习，天天向上",30, 50);
+    }
+}
---
+++ src/Day10/JavaApplet2.java
@@ -0,0 +1,14 @@
+package Day10;
+
+import java.awt.*;
+import java.applet.Applet;
+
+public class JavaApplet2 extends Applet {
+    Label prompt;
+    public void init() {
+        prompt = new Label("祝你学习顺利！");
+        prompt.setBackground(Color.yellow);
+        add(prompt);
+    }
+
+}
