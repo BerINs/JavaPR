93987c044a87d9bbe9afd1b166a7b0dd3fbde1e8
berior <1269921156@qq.com>
2020-11-07 01:08:11 +0800

    三角形

---
+++ src/Shapes/Triangle.java
@@ -0,0 +1,27 @@
+package Shapes;
+
+/**
+ * @author BerINs
+ * @date 11/7/20 12:40 AM
+ */
+
+
+public class Triangle extends Shapes{
+    protected int sideA;
+    protected int sideB;
+    protected int sideC;
+
+    public Triangle(int a, int b, int c) {
+        sideA = a;
+        sideB = b;
+        sideC = c;
+    }
+
+    public void setCircumference() {
+        circumference = sideA + sideB + sideC;
+    }
+    public void setArea() {
+        double p = this.getCircumference()/2;
+        area = Math.sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+    }
+}
